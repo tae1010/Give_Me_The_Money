2a3faa2b2d3169720507efdbc3ab46702494bffa
tae1010 <tae101010@naver.com>
2024-03-01 20:43:00 +0900

    model 생성

---
+++ Give_Me_The_Money/Model/Calculate.swift
@@ -0,0 +1,12 @@
+//
+//  Calculate.swift
+//  Give_Me_The_Money
+//
+//  Created by 김정태 on 3/1/24.
+//
+
+import Foundation
+
+struct Calculate {
+    
+}
---
+++ Give_Me_The_Money/Model/Users.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Give_Me_The_Money
+//
+//  Created by 김정태 on 3/1/24.
+//
+
+import Foundation
+
+struct Users {
+    var name: String
+}
+
+struct PreSet {
+    
+}
